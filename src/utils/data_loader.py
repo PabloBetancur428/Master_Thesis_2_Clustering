248cc1f579ebdaaa16963daeccd7f0c3bf23be0f
PabloBetancur428 <pablo1326699@gmail.com>
2025-04-10 16:54:21 +0200

    Removing deleted files

--- src/utils/data_loader.py
+++
@@ -1,8 +0,0 @@
-import nibabel as nib
-
-def load_nifti(path):
-    image = nib.load(path)
-    data = image.get_fdata()
-    affine = image.affine
-    header = image.header
-    return data, affine, header
